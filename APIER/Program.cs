781fc9fe7462623cdbb6b0fca2bab1f1f142b3ad
Teknokz <60339592+Teknokz@users.noreply.github.com>
2021-02-23 12:42:25 +0100

    initial

---
+++ APIER/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace APIER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+
+            System.Console.WriteLine("Vilken Pokemon vill du skaffa information för?");
+
+            RestClient client = new RestClient("https://pokeapi.co/api/v2/");
+
+            RestRequest request =new RestRequest("pokemon/snorlax");
+
+            IRestResponse response = client.Get(request);
+
+
+            System.Console.WriteLine(response.Content);
+
+            Console.ReadLine();
+        }
+    }
+}
